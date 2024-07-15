a7a034ca280f39bd1493e00f4dd5f315dfd4590b
jeepcreep <nilsweber@gmx.de>
2024-07-15 16:53:15 +0200

    added privacy policy

--- components/About.jsx
+++ components/About.jsx
@@ -12,6 +12,12 @@ const About = () => {
           </p>
         </section>
         <section className='w-full flex-center flex-col my-3'>
+          <h2 className='subhead2_text'>The Mission</h2>
+          <p className='desc_medium text-center text-gray-600'>
+            To make georeferencing easy and accessible to everyone.
+          </p>
+        </section>
+        <section className='w-full flex-center flex-col my-3'>
           <h2 className='subhead2_text'>Legal Information</h2>
           <p className='desc_medium text-center text-gray-600'>
             Built with the help of a few amazing open source libs, license files are/will be included. If you feel something is missing, please just reach out to me!
@@ -24,9 +30,19 @@ const About = () => {
           </p>
         </section>
         <section className='w-full flex-center flex-col my-3'>
-          <h2 className='subhead2_text'>The Mission</h2>
+          <h2 className='subhead2_text'>Privacy policy</h2>
           <p className='desc_medium text-center text-gray-600'>
-            To make georeferencing easy and accessible to everyone.
+            This website uses certain parts of your Google account to create a profile.
+          </p>
+          <p className='desc_medium text-center text-gray-600'>
+            These parts are: your name to show to other users potentially in the future (not yet activated), 
+            your e-mail address to create the actual account (aka create a unique login identifier internally)
+            as well as your profile picture to show on the top right side of the website as well as on your profile page (not yet activated).
+          </p>
+          <p className='desc_medium text-center text-gray-600'>
+            All of the information mentioned are needed in order to log you in, create an account for you
+            so you can come back and have your lovely maps persisted. None of your information will be used
+            anything other than realizing the pure functionality of georaffe.org.
           </p>
         </section>
       </div>
